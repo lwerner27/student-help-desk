4d62a596dff28c0e321d2c264246174f6cce80bf
Lucas Werner <lwerner27@gmail.com>
2020-11-03 12:17:53 -0600

    Created script collecting form data

---
+++ public/js/submit_request_form.js
@@ -0,0 +1,17 @@
+$(() => {
+    console.log("Document is ready.")
+
+    $("#submit-request-form").on("click", (event) => {
+        event.preventDefault()
+
+        // Object for holding info from the form 
+        let reqInfo ={};
+        reqInfo.fullName = $("#fullName").val()
+        reqInfo.email = $("#emailAddress").val()
+        reqInfo.phoneNumber = $("#phoneNumber").val()
+        reqInfo.studentFullName = $("#studentFullName").val()
+        reqInfo.school = $("#studentSchool").val()
+        reqInfo.issueDescription = $("#issueDescription").val()
+
+    })
+})
