3f3e1e6c4cb9f03b2226cd7243de8de838b78f5d
Lucas Werner <lwerner27@gmail.com>
2020-11-10 12:38:42 -0600

    Setup nodemailer transport and message configuration.

--- router/index.js
+++ router/index.js
@@ -1,4 +1,13 @@
+const nodemailer = require("nodemailer");
 const router = require("express").Router();
+
+let transporter = nodemailer.createTransport({
+    service: "Zoho",
+    auth: {
+        user: process.env.HELPDESK_EMAIL,
+        pass: process.env.HELPDESK_EMAIL_PASSWORD,
+    },
+});
 
 router.get("/", (req, res) => {
     res.render("request-form", { pageTitle: "Help Request Form" });
@@ -9,8 +18,20 @@ router.get("/success", (req, res) => {
 });
 
 router.post("/submit_form", (req, res) => {
-    console.log(req.body);
-    return res.status(200).send({ success: true, msg: "Success" });
+    const message = {
+        from: `${process.env.HELPDESK_EMAIL}`,
+        to: `${process.env[req.body.school]}`,
+        subject: `New Help Request from: ${req.body.fullName}`,
+        text: req.body.issueDescription,
+    };
+    transporter.sendMail(message, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Email has been sent.");
+            return res.status(200).send({ success: true, msg: "Success" });
+        }
+    });
 });
 
 module.exports = router;
