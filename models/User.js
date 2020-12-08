3a5a5e10e753fa41cafa726bf5875b18b236058b
Lucas Werner <34554883+lwerner27@users.noreply.github.com>
2020-12-07 20:20:44 -0600

    Setup user model so passwords hare auto hashed pre-save.

--- models/User.js
+++ models/User.js
@@ -1,10 +1,31 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const schema = new mongoose.Schema({
     fullName: "string",
     email: "string",
     password: "string",
     role: "string",
+});
+
+schema.pre("save", function (next) {
+    let user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified("password")) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 const User = mongoose.model("User", schema);
